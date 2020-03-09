095cccc622f2f456c29e14fcc95c5b9f42dbea52
yunju <sikka2034@gmail.com>
2020-03-09 20:46:39 +0900

    인자값 두개만 넘길 때

--- CSharpStudy/CSharpStudy/Program.cs
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -10,7 +10,60 @@ namespace CSharpStudy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello23");
+            int result;
+            while (true)
+            {
+                Console.WriteLine("부호를 입력하시요>>");
+                string sign = Console.ReadLine();
+
+                Console.WriteLine("인자값1>>");
+                string num1 = Console.ReadLine();
+                int number1 = int.Parse(num1);
+
+                Console.WriteLine("인자값2>>");
+                string num2 = Console.ReadLine();
+                int number2 = int.Parse(num2);
+
+                if (sign == "+")
+                {
+
+                    result = number1 + number2;
+
+                    Console.WriteLine("결과>>{0}", result);
+
+                }
+                if (sign == "-")
+                {
+                    result = number1 - number2;
+
+                    Console.WriteLine("결과>>{0}", result);
+                }
+
+                if (sign == "*")
+                {
+                    result = number1 * number2;
+
+                    Console.WriteLine("결과>>{0}", result);
+                }
+
+                if (sign == "/")
+                {
+                    result = number1 / number2;
+
+                    Console.WriteLine("결과>>{0}", result);
+                }
+                if (sign == "^")
+                {
+                    result = (int)Math.Pow(number1, number2);
+
+                    Console.WriteLine("결과>>{0}", result);
+                }
+                if (sign == "")
+                {
+
+                }
+
+            }
         }
     }
 }
