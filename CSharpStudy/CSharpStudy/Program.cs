61f7116fed16fbddbe94098313c5f65872a14645
yunju <sikka2034@gmail.com>
2020-03-10 16:12:28 +0900

    오류값잡기(완벽하진 않지만..!)

--- CSharpStudy/CSharpStudy/Program.cs
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CSharpStudy
 {
     class Program
     {
+      
+        
         static void Main(string[] args)
         {
             int result;
@@ -16,14 +19,20 @@ namespace CSharpStudy
                 Console.WriteLine("부호를 입력하시요>>");
                 string sign = Console.ReadLine();
 
-                Console.WriteLine("인자값1>>");
-                string num1 = Console.ReadLine();
-                int number1 = int.Parse(num1);
-
+              
+                    Console.WriteLine("인자값1>>");
+                    string num1 = Console.ReadLine();
+                    int number1;
+      
                 Console.WriteLine("인자값2>>");
                 string num2 = Console.ReadLine();
-                int number2 = int.Parse(num2);
+                int number2;
+                if ((!Int32.TryParse(num1, out number1))|(!Int32.TryParse (num2, out number2)))
+                {
+                    Console.WriteLine("오류!!숫자로 입력하세요!");
 
+                }
+              
                 if (sign == "+")
                 {
 
