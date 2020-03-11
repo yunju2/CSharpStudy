981e33e5eb6f4a1b40ece46d3413b461f216df85
yunju <sikka2034@gmail.com>
2020-03-11 18:48:57 +0900

    숙제1끝

--- CSharpStudy/CSharpStudy/Program.cs
+++ CSharpStudy/CSharpStudy/Program.cs
@@ -29,61 +29,111 @@ namespace CSharpStudy
                     break;
                 case "*":
                     {
-                        tmp = number1 * number2;
+                        tmp = mul(number1 , number2);
                         result = tmp.ToString();
 
                     }
                     break;
                 case "/":
                     {
-                        tmp = number1 / number2;
+                        tmp = div(number1 , number2);
                         result = tmp.ToString();
                     }
                     break;
                 case "^":
                     {
-                        tmp = (int)Math.Pow(number1, number2);
+                        tmp = inv(number1, number2);
                         result = tmp.ToString();
                     }
                     break;
 
                 case "<<":
                     {
-                        tmp = number1 << number2;
+                        tmp = shift(number1,number2);
                         result = tmp.ToString();
                     }
                     break;
 
-                case "root":
-                    //result = (int)Math.Sqrt();
-                    break;
-
-                case "++":
-                    break;
-                
-                case "--":
-                    break;
 
                 default:
-                    result = "잘못된 값입니다. 다시 입력해주세요";
+                    result = "잘못된 값;입니다. 다시 입력해주세요";
                     break;
             }
 
             print(result);
             //return result;
         }
+            void calculate1(string sign,int number1)
+            {
+                int tmp;
+                string result = "";
+            switch (sign)
+            {
+                case "root":
+                    {
+                        tmp = root(number1);
+                        result = tmp.ToString();
+                        break;
+                    }
+                case "++":
+                    {
+                        tmp = addmove(number1);
+                        result = tmp.ToString();
+                        break;
+                    }
+                case "--":
+                    {
+                        tmp = submove(number1);
+                        result = tmp.ToString();
+
+                        break;
+                    }
+                default:
+                    result = "잘못된 값;입니다. 다시 입력해주세요";
+                    break;
+            }
+            print(result);
+            }
 
         int add(int a, int b)
         {
             // int add.a = number1;
             // int add.b = number2;
-            // int temp = add.a + add.b;
+            // int temp = add.a + add.b;s
             // result1 = temp;
             return a + b;
         }
         int sub(int a, int b)
         {
             return a - b;
+        }
+        int div(int a, int b)
+        {
+            return a / b;
+        }
+        int mul (int a, int b)
+        {
+            return a * b;
+        }
+        int inv(int a, int b)
+        {
+            return (int) Math.Pow(a, b);
+        }
+        int shift(int a, int b)
+        {
+            return a << b;
+        }
+        int root(int a)
+        {
+            return (int)Math.Sqrt(a);
+        }
+        int addmove(int a)
+        {
+            return ++a;
+        }
+        int submove(int a)
+        {
+            return --a;
         }
 
         void print(int result)
@@ -98,7 +148,7 @@ namespace CSharpStudy
 
         public void run()
         {
-            int result1;
+            
 
             while (true)
             {
@@ -110,16 +160,35 @@ namespace CSharpStudy
                     Console.Clear();
                     continue;
                 }
-
-                Console.WriteLine("인자값1>>");
-                string num1 = Console.ReadLine();
-                int number1;
-                if (!Int32.TryParse(num1, out number1))
+                if (sign.ToLower() == "end")
                 {
-                    Console.WriteLine("오류!! 1번 인자값을 숫자를 입력하세요!");
-                    continue;
+                    break;
                 }
 
+                    Console.WriteLine("인자값1>>");
+                    string num1 = Console.ReadLine();
+                    int number1;
+                    if (!Int32.TryParse(num1, out number1))
+                    {
+                        Console.WriteLine("오류!! 1번 인자값을 숫자를 입력하세요!");
+                        continue;
+                    }
+                if (sign=="root")
+                {  calculate1(sign, number1);
+                    continue;
+
+                }
+                else if (sign == "++")
+                {
+                    calculate1(sign, number1);
+                    continue;
+
+                }
+                else if (sign == "--")
+                {
+                    calculate1(sign, number1);
+                    continue;
+                }
                 Console.WriteLine("인자값2>>");
                 string num2 = Console.ReadLine();
                 int number2;
@@ -129,11 +198,11 @@ namespace CSharpStudy
                     continue;
                 }
 
-                if (sign.ToLower() == "end")
-                    break;
+                    
 
                 calculate(sign, number1, number2);
             }
+                
         }
 
         class Program
