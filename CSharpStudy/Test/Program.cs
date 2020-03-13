c323354a4176b179ec8c8d5077b6cb234084f4dd
yunju <sikka2034@gmail.com>
2020-03-13 17:42:53 +0900

    for문 *만들기

--- CSharpStudy/Test/Program.cs
+++ CSharpStudy/Test/Program.cs
@@ -9,8 +9,34 @@ namespace Test
 {
     class Program
     {
+        
+       static int pow(int a, int b)
+      {
+            //2 ^ 3 -> 1 * 2 * 2 * 2
+            //2 ^ 4 -> 1 * 2 * 2 * 2 * 2
+            for (int i = 0; i < b; ++i)
+            {
+
+                a *= a;
+            }
+
+            int sum = 1;
+            for (int i = 1; i <= 3; ++i)
+            {
+                sum *= 2;
+            }
+
+            return a;
+        }
+
+
+
+    
+       
         static void Main(string[] args)
         {
+            int tmp = pow(2, 3);
+            Console.WriteLine(tmp);
         }
     }
 }
